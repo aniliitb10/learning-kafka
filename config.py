b45bafe98de91bc698e3d59e45388047a9a99db0
Anil Kumar <aniliitb10@gmail.com>
2022-11-15 09:04:15 +0530

    Rename md_producer.py and add more tickers

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
 
-TICKERS: List[str] = ["SBIN", "RELIANCE", "ADANIENT", "ICICIBANK"]
-TICKERS_PRICES: Dict[str, float] = {"SBIN": 600, "RELIANCE": 2630, "ADANIENT": 4006, "ICICIBANK": 907}
+TICKERS: List[str] = ["SBIN", "RELIANCE", "ADANIENT", "ICICIBANK", "TATASTEEL", "TCS"]
+TICKERS_PRICES: Dict[str, float] = {"SBIN": 600, "RELIANCE": 2630, "ADANIENT": 4006, "ICICIBANK": 907,
+                                    "TATASTEEL": 108, "TCS": 3332}
